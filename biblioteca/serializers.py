0b063412b9c74e9a356f24457765c2036e015db7
Israel <israelbr@ufps.edu.co>
2025-11-12 19:21:42 -0500

    Arreglo

--- biblioteca/serializers.py
+++ biblioteca/serializers.py
@@ -14,7 +14,7 @@ class LibroSerializer(serializers.ModelSerializer):
     # Calculamos el atributo, pasamos el obj que es lo que se serializa
     def get_recent_reviews(self, obj):
         # Ordena las reseñas del objeto de forma descendente mostrando los 5 primeros
-        reviews = obj.review_set.order_by('-fecha')[:5]
+        reviews = obj.resenas.order_by('-fecha')[:5]
         # Retornar las reseñas del libro como coleccion de objetos y lo convertirmos a formato serializer .data
         return ResenaSerializer(reviews, many=True).data
     
